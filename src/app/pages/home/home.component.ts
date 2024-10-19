79945767565e98d3bb55b86b9a048193a649973a
Andre <lulesaaso2@gmail.com>
2024-10-18 20:38:39 -0500

    update-slider-HomeComponent

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,15 +9,26 @@ import { PromService } from '../../services/prom.service';
 export class HomeComponent implements OnInit{
 
   promList:any=[];
+  currentSlide:number = 0;
+  totalSlides: number = 3;
 
   constructor(private promService:PromService){
   }
 
   ngOnInit(): void {
+    this.autoSlide()
     this.promService.findAll().subscribe(
       (data:any)=>{
         this.promList = data;
       }
     )
+  }
+  autoSlide():void{
+    setInterval(()=>{
+      this.currentSlide = (this.currentSlide + 1) % this.totalSlides;//El %(modulo) fuciona como freno para que cuando se llegue al ultimo slider se regrese al primer slider ej 2 + 1 = 3 // 3 % 3 = 0 este es el index
+    },5000)
+  }
+  goToSlide(index:number){
+    this.currentSlide = index;
   }
 }
